b6a1a7830a5c421db3f6d9ef4df423abe135c987
Xavier <xavier01242@gmail.com>
2021-05-14 20:40:18 +0200

    Seccion dos culumnas

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const leftArrow = document.querySelector('.carousel__control--left');
 const rightArrow = document.querySelector('.carousel__control--right');
 const indicators = Array.from(document.querySelector('.carousel__indicators').children);
 let slideCounter = 0;
-
+let mouseEncima = false;
 
 /******************Utilities*********************/
 
@@ -13,8 +13,28 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+document.querySelector('.carousel').addEventListener('mouseover', function() {
+    mouseEncima = true;
+});
+
+document.querySelector('.carousel').addEventListener('mouseout', function() {
+    mouseEncima = false;
+});
+
+function reset(subject, clas) {
+    if (slideCounter < 0) {
+        slideCounter = 2;
+    }
+
+    for (let i = 0; i < subject.length; i++) {
+        subject[i].forEach(item => item.classList.remove(clas[i]));
+        subject[i][Math.abs(slideCounter % subject[0].length)].classList.add(clas[i]);
+    }
+}
+
 
 /******************Start******************/
+
 window.onload = function() {
     startCarousel();
 }
@@ -22,8 +42,10 @@ window.onload = function() {
 async function startCarousel() {
     while (true) {
         await sleep(13000);
-        slideCounter++;
-        reset([carouselItems, indicators], ['visible', 'active']);
+        if (!mouseEncima) {
+            slideCounter++;
+            reset([carouselItems, indicators], ['visible', 'active']);
+        }
     }
 }
 
@@ -45,14 +67,3 @@ indicators.forEach(function(item, index) {
         reset([carouselItems, indicators], ['visible', 'active']);
     }
 });
-
-function reset(subject, clas) {
-    if (slideCounter < 0) {
-        slideCounter = 2;
-    }
-
-    for (let i = 0; i < subject.length; i++) {
-        subject[i].forEach(item => item.classList.remove(clas[i]));
-        subject[i][Math.abs(slideCounter % subject[0].length)].classList.add(clas[i]);
-    }
-}
