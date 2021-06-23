e44a504e4ad887780957678fe9e232b6aa2b088e
Xavier <xavier01242@gmail.com>
2021-06-23 18:16:58 +0200

    Pagina de test creada solo frontend

---
+++ test.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <title>Test</title>
+    <link rel="icon" href="img/shortcut-icon.svg">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width = device-width, initial-scale=1.0">
+    <meta name="description" content="Empresa dedicada a la inovación médica">
+    <meta name="keywords" content="genetica, cromosomas, investigacion, ciencia">
+    <meta name="author" content="Xavier Lliteras">
+    <meta property="og:title" content="Test">
+    <meta property="og:description" content="¡Find out how much you know!">
+    <meta property="og:image" content="img/carousel_3.jpg">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="js/utilities.js" defer></script>
+</html>
+
+<body>
+    <nav class="nav">
+        <div class="container flex">
+            <div class="mobile-flex">
+                <div class="nav__logo">
+                    <a href="index.html"><img src="img/logo.svg" alt="logo-regenite" class="nav__logo-img"></a>
+                </div>
+                <div class="nav__hamburger">
+                    <img src="img/hamburger.svg" alt="display menu icon">
+                </div>
+            </div>
+            <ul class="nav__menu">
+                <li class="nav__item">
+                    <a href="index.html" class="nav__link">Home</a>
+                </li>
+                <li class="nav__item">
+                    <a href="#" class="nav__link">Contact</a>
+                </li>
+                <li class="nav__item">
+                    <a href="tests.php" class="nav__link">Tests</a>
+                </li>
+                <li class="nav__item">
+                    <a href="#" class="nav__button white">DONATE</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    
+</body>
