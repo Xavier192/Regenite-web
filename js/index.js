f309b36af57de7046249e0ff07da6827c4cc5cb5
Xavier <xavier01242@gmail.com>
2022-03-09 22:12:15 +0100

    Implemented carousel

---
+++ js/index.js
@@ -0,0 +1,73 @@
+const newsCarousel = function (){
+    /*DOM OBJECTS*/
+    const leftButton = document.querySelector('.news__control--left');
+    const rightButton = document.querySelector('.news__control--right');
+    const carouselContainer = document.querySelector('.news__slider');
+    const indicators = document.querySelectorAll('.news__indicator');
+    let width = carouselContainer.clientWidth;
+    let sliderCount = 1;
+    let indicatorCount = 0;
+
+    carouselContainer.style.transform = `translateX(-${(width +20) * sliderCount}px)`;
+
+    window.onresize = function(){
+        width = carouselContainer.clientWidth;
+        carouselContainer.style.transform = `translateX(-${(width +20) * sliderCount}px)`;
+    }
+
+    leftButton.onclick = function(){
+        sliderCount--;
+        indicatorCount--;
+
+        if(sliderCount < 0){
+            return;
+        }
+        
+        if(indicatorCount >= 0){
+            indicators[(indicatorCount +1)].classList.remove('news__indicator--active');
+            indicators[indicatorCount].classList.add('news__indicator--active');
+        }
+        
+        moveCarousel();
+    }
+
+    rightButton.onclick = function(){
+        sliderCount++;
+        indicatorCount++;
+
+        if(sliderCount > carouselContainer.children.length -1){
+            return;
+        }
+
+        if((indicatorCount-1) < carouselContainer.children.length -3){
+            indicators[(indicatorCount -1)].classList.remove('news__indicator--active');
+            indicators[indicatorCount].classList.add('news__indicator--active');
+        }
+        
+        moveCarousel();
+    }
+
+    carouselContainer.addEventListener('transitionend', () => {
+        carouselContainer.style.transition = 'none';
+        if(sliderCount < 1){
+            sliderCount = carouselContainer.children.length -2; 
+            carouselContainer.style.transform = `translateX(-${(width +20) * sliderCount}px)`;
+            indicatorCount = carouselContainer.children.length -3;
+            indicators[indicatorCount].classList.add('news__indicator--active');
+            indicators[0].classList.remove('news__indicator--active');
+        }
+        if(sliderCount > carouselContainer.children.length -2){
+            sliderCount = 1;
+            carouselContainer.style.transform = `translateX(-${(width +20) * sliderCount}px)`;
+            indicatorCount = 0;
+            indicators[0].classList.add('news__indicator--active');
+            indicators[carouselContainer.children.length -3].classList.remove('news__indicator--active');
+        }
+    });
+
+    function moveCarousel(){
+        carouselContainer.style.transition = 'transform ease-in-out .85s';
+        carouselContainer.style.transform = `translateX(-${(width +20) * sliderCount}px)`;
+    }
+
+}();
